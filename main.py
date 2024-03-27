90fb87743d4aaa59a619b17c28fb56c778dd4f18
patrick <patrickdlacey@gmaillcom>
2024-03-26 19:21:48 -0700

    Fix delete method and add AboutDialog class with functionality

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ from PyQt6.QtCore import Qt
 import sqlite3
 
 
+class DatabaseConnection:
+    def __init__(self, database_file="database.db"):
+        self.database_file = database_file
+
+    def connect(self):
+        connection = sqlite3.connect(self.database_file)
+        return connection
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -70,7 +79,7 @@ class MainWindow(QMainWindow):
     def load_data(self):
         """Loads data from the database.db file and creates rows & columns based
         off the information provided"""
-        connection = sqlite3.connect("database.db")
+        connection = DatabaseConnection().connect()
         result = connection.execute("SELECT * FROM students")
         self.table.setRowCount(0)
         for row_number, row_data in enumerate(result):
@@ -106,7 +115,7 @@ class AboutDialog(QMessageBox):
         self.setWindowTitle("About")
         content = """
         This app was created during the Python course.
-        Feel free to modify adn reuse this app.
+        Feel free to modify and reuse this app.
         """
         self.setText(content)
 
@@ -155,7 +164,7 @@ class EditDialog(QDialog):
         self.setLayout(layout)
 
     def update_student(self):
-        connection = sqlite3.connect("database.db")
+        connection = DatabaseConnection().connect()
         cursor = connection.cursor()
         cursor.execute("UPDATE students SET name = ?, course = ?, mobile = ? WHERE id = ?",
                        (self.student_name.text(),
@@ -193,7 +202,7 @@ class DeleteDialog(QDialog):
         index = main_window.table.currentRow()
         student_id = main_window.table.item(index, 0).text()
 
-        connection = sqlite3.connect("database.db")
+        connection = DatabaseConnection().connect()
         cursor = connection.cursor()
         cursor.execute("DELETE from students WHERE id = ?", (student_id, ))
         connection.commit()
